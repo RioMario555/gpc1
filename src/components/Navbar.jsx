ff7cf8f87266a69435c79123cbd512c7fad478a4
RioMario555 <ira.chrelash@gmail.com>
2022-10-28 11:40:27 +0400

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import gpcImg from '../assets/logo.svg'
-import shopIcone from '../assets/shop.svg'
-import heartIcone from '../assets/heart.svg'
-import personIcone from '../assets/person.svg'
-
+import React from "react";
+import gpcImg from "../assets/logo.svg";
+import shopIcone from "../assets/shop.svg";
+import heartIcone from "../assets/heart.svg";
+import personIcone from "../assets/person.svg";
 
 const Navbar = () => {
   return (
     <div className="header">
-        <div className="home">
-            <img className="logo" src={gpcImg}/>
-        </div>
+      <div className="home">
+        <img className="logo" alt="logo" src={gpcImg} />
+      </div>
       <div className="menu">
         <ul>
           <li>პროდუქცია</li>
@@ -19,13 +18,13 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="login">
-        <img className="shop" src={shopIcone}/>
-        <img className="shop" src={heartIcone}/>
-        <img className="person" src={personIcone}/>
+        <img className="shop" alt="shop" src={shopIcone} />
+        <img className="shop" alt="shop" src={heartIcone} />
+        <img className="person" alt="shop" src={personIcone} />
         <p>კაბინეტი</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
