e34e8da314c2016c936c6b3cd9467efacbe929c8
iraa16 <92379384+RioMario555@users.noreply.github.com>
2023-03-02 13:34:31 +0400

    addmargin

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,12 +3,10 @@ import gpcImg from "../../assets/icons/logo.svg";
 import shopIcone from "../../assets/icons/shop.svg";
 import heartIcone from "../../assets/icons/heart.svg";
 import personIcone from "../../assets/icons/person.svg";
-import './Navbar.css'
-
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-
     <header>
       <div className="header">
         <div className="home">
@@ -20,13 +18,13 @@ const Navbar = () => {
             <li>ჩვენს შესახებ</li>
             <li>აფთიაქები</li>
           </ul>
+          <div className="login">
+            <img className="shop" alt="shop" src={shopIcone} />
+            <img className="shop" alt="shop" src={heartIcone} />
+            <img className="person" alt="shop" src={personIcone} />
+            <p>კაბინეტი</p>
+          </div>
         </nav>
-        <div className="login">
-          <img className="shop" alt="shop" src={shopIcone} />
-          <img className="shop" alt="shop" src={heartIcone} />
-          <img className="person" alt="shop" src={personIcone} />
-          <p>კაბინეტი</p>
-        </div>
       </div>
     </header>
   );
