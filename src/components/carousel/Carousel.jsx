250108e78b6ededafd98a103b5750c9a1e35596b
iraa16 <92379384+RioMario555@users.noreply.github.com>
2023-03-02 11:54:37 +0400

    updatemargin

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import './Carousel.css'
+import React from "react";
+import "./Carousel.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
-import 'swiper/css';
+import "swiper/css";
 import "swiper/css/pagination";
 import glovo from "../../assets/images/glovo.svg";
 
-
 const Carousel = () => {
   return (
-    <>
-    <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
-      <SwiperSlide><img src={glovo}/></SwiperSlide>
-      <SwiperSlide><img src={glovo}/></SwiperSlide>
-      <SwiperSlide><img src={glovo}/></SwiperSlide>
-    </Swiper>
-  </>
-  )
-}
+    <div className="glovo_carousel">
+      <>
+        <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+          <SwiperSlide>
+            <img src={glovo} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={glovo} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={glovo} />
+          </SwiperSlide>
+        </Swiper>
+      </>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
